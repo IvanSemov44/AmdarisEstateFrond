ad3ddf6f961296fc2d7138d41971071333c6f130
Иван Семов <ivan.semov.44@gmail.com>
2023-01-15 14:30:24 +0200

    Update pagin in EstateCatolog to work correct

--- ivanrealestatefrondend/src/Components/Estate/EstateCatalog/EstateCatalog.js
+++ ivanrealestatefrondend/src/Components/Estate/EstateCatalog/EstateCatalog.js
@@ -1,18 +1,18 @@
 import { useEffect, useState } from 'react';
-import { Pagination, Stack, Box } from '@mui/material';
+import { Pagination, Stack, Box, Typography, Grid } from '@mui/material';
 
 import EstateCard from '../EstateCard/EstateCard';
+import { Spinner } from '../../Common/Spinner/Spinner';
 
 import * as estatesSevice from '../../../Services/EstateService';
 
 const EstateCatalog = () => {
     const [estates, setEstate] = useState([]);
     const [pagin, setPagin] = useState([]);
-    const [page, setPage] = useState(pagin.CurrentPage);
+    const [page, setPage] = useState(1);
 
     const handleChange = (event, value) => {
         setPage(value);
-        console.log(page);
     };
 
     useEffect(() => {
@@ -28,17 +28,21 @@ const EstateCatalog = () => {
             ignore = true;
         };
     }, [page]);
+    console.log(pagin.CurrentPage);
 
     return (
         <Stack alignItems="center" spacing={2} >
             <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
                 {estates.map(x => <EstateCard key={x.estateId} estate={x} />)}
             </Box>
+            <Typography>Page: {page}</Typography>
             <Pagination
                 sx={{ margin: 3 }}
                 size="large"
                 count={pagin.TotalPages}
                 page={page}
+                variant="outlined"
+                color="primary"
                 onChange={handleChange}
             />
         </Stack >
